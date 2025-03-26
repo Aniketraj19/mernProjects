cdb2cbefe2de65eb3652f928fd7cf687ce1158e0
Aniketraj19 <aniketraj19verma@gmail.com>
2025-03-26 07:01:27 +0530

    created the show card option

--- simpleBussinessCard/frontend/src/App.jsx
+++ simpleBussinessCard/frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import { CreateBussinessCard } from "./components/CreateBussinessCard";
+import { ShowCards } from "./components/ShowCards";
+import { useState } from "react";
 
 function App() {
+  const [ans, setAns] = useState([]);
+  fetch("http://localhost:3000/cards")
+    .then((res) => res.json())
+    .then((data) => {
+      setAns(data.response);
+    });
   return (
     <div>
       <CreateBussinessCard />
+      {ans.map(function (card, index) {
+        return (
+          <ShowCards
+            index={index}
+            name={card.name}
+            description={card.description}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ simpleBussinessCard/frontend/src/components/ShowCards.jsx
@@ -0,0 +1,10 @@
+export function ShowCards({ name, description }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{description}</h2>
+    </div>
+  );
+}
+
+// 34 minutes
